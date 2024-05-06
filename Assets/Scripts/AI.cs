79c2163bb218a0c8f56aeaec2cfc47872f7417d2
Sean Duggan <sean.duggan@gmail.com>
2024-05-06 18:05:11 -0400

    Set up basic enemy running from start to end

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AI : MonoBehaviour
+{
+    NavMeshAgent _navMeshAgent;
+
+    [SerializeField] Transform _startPoint;
+    [SerializeField] Transform _endPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        transform.position = _startPoint.position;
+
+        _navMeshAgent.destination = _endPoint.position;
+    }
+}
